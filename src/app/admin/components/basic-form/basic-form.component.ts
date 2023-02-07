45eac8ebb50d0293d7c10c1170f6666afa7d822e
Chanelp <mariannissanchez27@gmail.com>
2023-02-07 10:43:59 -0400

    Dominando el FormControl y sus estados

---
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
+
+@Component({
+  selector: 'app-basic-form',
+  templateUrl: './basic-form.component.html',
+  styleUrls: ['./basic-form.component.scss']
+})
+export class BasicFormComponent implements OnInit {
+
+  //Crear un Form Control
+  nameField = new FormControl('Aprendiendo Reactive Forms');
+
+  constructor() { }
+
+  ngOnInit(): void {
+    // Obtener un observable del valor
+    // this.nameField.valueChanges.subscribe(value => {
+    //   console.log(value);
+    // });
+  }
+
+  getNameValue(){
+    console.log(this.nameField.value);
+  }
+
+}
