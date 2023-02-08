802a3af33b542dd1a329fd87d5bd85e9c4fd9eb8
Chanelp <mariannissanchez27@gmail.com>
2023-02-08 15:40:59 -0400

    FormGroup para agrupar multiples campos

--- src/app/admin/components/basic-form/basic-form.component.ts
+++ src/app/admin/components/basic-form/basic-form.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, Validators } from '@angular/forms';
+import { FormControl, Validators, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-basic-form',
@@ -8,43 +8,107 @@ import { FormControl, Validators } from '@angular/forms';
 })
 export class BasicFormComponent implements OnInit {
 
-  //Crear un Form Control -inputs
-  nameField = new FormControl('', [Validators.required, Validators.maxLength(10)]);
-  emailField = new FormControl('');
-  phoneField = new FormControl('');
-  colorField = new FormControl('');
-  dateField = new FormControl('');
-  numberField = new FormControl('');
-  searchField = new FormControl('');
-  imageField = new FormControl('');
-  // Selects
-  categoryField = new FormControl('tipo-1');
-  multiplytagField = new FormControl('');
-  // Radio y checkbox
-  agreeField = new FormControl(false);
-  genderField = new FormControl();
-  // Preferencias
-  preferencias = new FormControl('');
+  form = new FormGroup({
+    name: new FormControl('', [Validators.required, Validators.maxLength(10)]),
+    email: new FormControl(''),
+    phone: new FormControl(''),
+    color: new FormControl('#000000'),
+    date: new FormControl(''),
+    id: new FormControl(''),
+    search: new FormControl(''),
+    image: new FormControl(''),
+    category: new FormControl('tipo-1'),
+    multiply: new FormControl(''),
+    agree: new FormControl(false),
+    gender: new FormControl(false),
+    preferencias: new FormControl('')
+  });
+
+  valores = this.form.value;
+  formValue = JSON.parse(localStorage.getItem('Form'))
 
   constructor() { }
 
   ngOnInit(): void {
-    // Obtener un observable del valor
-    this.nameField.valueChanges.subscribe(value => {
+    this.nameField.valueChanges
+    .subscribe(value => {
       console.log(value);
     });
   }
 
-  getNameValue(){
-    console.log(this.nameField.value);
+  // Save in LocalStorage
+  save(event: Event) {
+    if(this.form.invalid) {
+      this.form.controls.name.markAllAsTouched();
+
+      return;
+    }
+
+    this.valores = this.form.value;
+    localStorage.setItem(`Form${this.valores.id}`, JSON.stringify(this.valores));
+    console.log(this.formValue);
   }
 
-  get isnameFieldValid(){
+  get nameField(){
+    return this.form.get('name');
+  }
+
+  get isNameFieldValid() {
     return this.nameField.touched && this.nameField.valid;
   }
 
-  get isnameFieldInvalid(){
+  get isNameFieldInvalid() {
     return this.nameField.touched && this.nameField.invalid;
   }
 
+  get emailField() {
+    return this.form.get('email');
+  }
+
+  get phoneField() {
+    return this.form.get('phone');
+  }
+
+  get colorField() {
+    return this.form.get('color');
+  }
+
+  get dateField() {
+    return this.form.get('date');
+  }
+
+  get idField() {
+    return this.form.get('id');
+  }
+
+  get searchField() {
+    return this.form.get('search');
+  }
+
+  get imageField() {
+    return this.form.get('image');
+  }
+
+  get categoryField() {
+    return this.form.get('category');
+  }
+
+  get tagField() {
+    return this.form.get('multiply');
+  }
+
+  get agreeField() {
+    return this.form.get('agree');
+  }
+
+  get genderField() {
+    return this.form.get('gender');
+  }
+
+  get preferenciasField() {
+    return this.form.get('preferencias');
+  }
+
 }
+
+
