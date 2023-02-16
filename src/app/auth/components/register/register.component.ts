610d3e6bf68dceb7d441bfab8974088d47269535
Chanelp <mariannissanchez27@gmail.com>
2023-02-16 12:53:09 -0400

    Validación condicionada y reactividad a variaciones en la UI

--- src/app/auth/components/register/register.component.ts
+++ src/app/auth/components/register/register.component.ts
@@ -1,5 +1,9 @@
 import { Component, OnInit } from '@angular/core';
-import { UntypedFormBuilder, UntypedFormGroup, Validators } from '@angular/forms';
+import {
+  UntypedFormBuilder,
+  UntypedFormGroup,
+  Validators,
+} from '@angular/forms';
 import { Router } from '@angular/router';
 
 import { AuthService } from './../../../core/services/auth.service';
@@ -9,10 +13,9 @@ import { MyValidators } from '../../../utils/validators';
 @Component({
   selector: 'app-register',
   templateUrl: './register.component.html',
-  styleUrls: ['./register.component.scss']
+  styleUrls: ['./register.component.scss'],
 })
 export class RegisterComponent implements OnInit {
-
   form: UntypedFormGroup;
 
   constructor(
@@ -23,29 +26,49 @@ export class RegisterComponent implements OnInit {
     this.buildForm();
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
   register(event: Event) {
     event.preventDefault();
     if (this.form.valid) {
       const value = this.form.value;
-      this.authService.createUser(value.email, value.password)
-      .then(() => {
+      this.authService.createUser(value.email, value.password).then(() => {
         this.router.navigate(['/auth/login']);
       });
     }
   }
 
   private buildForm() {
-    this.form = this.formBuilder.group({
-      email: ['', [Validators.required]],
-      password: ['', [Validators.required]],
-      confirmPassword: ['', [Validators.required]],
-    }, {
-      validators: MyValidators.matchPasswords
-    }
+    this.form = this.formBuilder.group(
+      {
+        email: ['', [Validators.required]],
+        password: ['', [Validators.required]],
+        confirmPassword: ['', [Validators.required]],
+        type: ['', Validators.required],
+        companyName: ['', Validators.required],
+      },
+      {
+        validators: MyValidators.matchPasswords,
+      }
     );
+
+    this.typeField.valueChanges.subscribe((value) => {
+      console.log(value);
+      if(value === 'company'){
+        this.companyNameField.setValidators([Validators.required]);
+      } else {
+        this.companyNameField.setValidators(null);
+      }
+
+      this.companyNameField.updateValueAndValidity();
+    });
   }
 
+  get typeField() {
+    return this.form.get('type');
+  }
+
+  get companyNameField() {
+    return this.form.get('companyName');
+  }
 }
